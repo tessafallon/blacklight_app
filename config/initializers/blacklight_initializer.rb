5791d207c20a7137523c59f9865d44eeeb9dd56f
contessa <tessa.fallon@gmail.com>
2015-01-22 02:02:00 -0500

    Use sprockets-rails 2.1.3 to avoid weird asset erros

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '2f3fba42e60b29bfc5382a9ba16badb373ce3293f30923506b5d48a84b624b3ebcf9098b5755ecedd83b211eedfad465e69ceac271ff935cb17c18561cec099a'
+
